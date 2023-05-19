ebec5f68dcbfc0723959704dd2d4d99f38966fac
devjldp diaz <joseluisdp297@gmail.com>
2023-05-19 08:33:30 +0000

    exercise finisehd

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,40 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumber(number1, number2){
+//     return number1+number2;
+// }
+// let sum = addTwoNumber(3,5)
+// console.log(sum)
+
 // Arrow Function With Parameters
+
+// const addTwoNumber = (number1, number2) => {
+//     return number1+number2;
+// };
+
+// let sum2 = addTwoNumber(3,5);
+// console.log(sum2);
 
 // Single Line Arrow Function With Parameters
 
+// const addThreeNumbers = (num1,num2,num3) => num1+num2+num3;
+// let sum2 = addThreeNumbers(3,4,1);
+// console.log(sum2);
+
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hi There!");
+
+const sayHello = () => console.log("hello!");
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiple line!
+    </p>`
+)
+
+console.log(returnMultipleLines());
